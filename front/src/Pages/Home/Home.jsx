18748166a9bf35ddbabb13add2d64191187e4457
Marvindlls <marvindillenseger@gmail.com>
2024-12-09 16:51:14 +0100

    add marmiton-api dependency and update Home component layout

--- front/src/Pages/Home/Home.jsx
+++ front/src/Pages/Home/Home.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
 import Navbar from "../../components/header/Navbar";
+import logo from '../../assets/react.svg';
+import './Home.scss';
 
 function Home () {
     return(
         <div>
             <Navbar />
+            <div className="container">
+                <img src={logo} alt="" />
+                <img src={logo} alt="" />
+                <img src={logo} alt="" />
+                <img src={logo} alt="" />
+                <img src={logo} alt="" />
+                <img src={logo} alt="" />
+                <img src={logo} alt="" />
+            </div>
         </div>
     )
 }
