048b4570b3edbcf2956408ee5666ceb8f0ad8bd2
Marvindlls <marvindillenseger@gmail.com>
2024-12-11 13:28:27 +0100

    add filter button to Home component and enhance styling for improved user experience

--- front/src/Pages/Home/Home.jsx
+++ front/src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import './Home.scss';
+import Arrow from '../../assets/arrow.svg';
 import Modal from "../../components/modal/modal";
 
 function Home({ isModalOpen, setIsModalOpen }) {
-    const [recipes, setRecipes] = useState([]); // État pour les recettes
+    const [recipes, setRecipes] = useState([]);
 
     const addRecipe = (newRecipe) => {
         setRecipes((prevRecipes) => [...prevRecipes, newRecipe]);
@@ -12,13 +13,13 @@ function Home({ isModalOpen, setIsModalOpen }) {
     return (
         <main className="home">
             
-            
+            <button className="first">Filtrer <img src={Arrow} alt="flèche du bas" /></button>
             <div className="home__list">
                 {recipes.map((recipe, index) => (
                     <div key={index} className="recipe-card">
                         {recipe.image && <img src={recipe.image} alt={recipe.title} />}
                         <h3>{recipe.title}</h3>
-                        <p>Temps de préparation: {recipe.timing} min</p>
+                        <p>Temps de préparation: {recipe.timing}</p>
                         <button>Voir plus</button>                        
                     </div>
                 ))}
@@ -27,7 +28,7 @@ function Home({ isModalOpen, setIsModalOpen }) {
             <Modal 
                 isOpen={isModalOpen} 
                 onClose={() => setIsModalOpen(false)} 
-                onSubmit={addRecipe} // Passe la fonction à la modale
+                onSubmit={addRecipe}
             />
         </main>
     );
