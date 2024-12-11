0f47954aef62b0f9026c85c5623b87d01ec25383
Marvindlls <marvindillenseger@gmail.com>
2024-12-11 13:50:15 +0100

    add favorite functionality to recipes in Home component and update styles for better UI

--- front/src/Pages/Home/Home.jsx
+++ front/src/Pages/Home/Home.jsx
@@ -1,23 +1,38 @@
 import { useState } from "react";
 import './Home.scss';
 import Arrow from '../../assets/arrow.svg';
+import Favori from '../../assets/fill.svg';
+import Favori2 from '../../assets/empty.svg';
 import Modal from "../../components/modal/modal";
 
 function Home({ isModalOpen, setIsModalOpen }) {
     const [recipes, setRecipes] = useState([]);
+    const [favorites, setFavorites] = useState({}); // Gère les favoris (key: index, value: boolean)
 
     const addRecipe = (newRecipe) => {
         setRecipes((prevRecipes) => [...prevRecipes, newRecipe]);
     };
 
+    const toggleFavorite = (index) => {
+        setFavorites((prevFavorites) => ({
+            ...prevFavorites,
+            [index]: !prevFavorites[index], // Alterne le statut du favori
+        }));
+    };
+
     return (
         <main className="home">
-            
             <button className="first">Filtrer <img src={Arrow} alt="flèche du bas" /></button>
             <div className="home__list">
                 {recipes.map((recipe, index) => (
                     <div key={index} className="recipe-card">
                         {recipe.image && <img src={recipe.image} alt={recipe.title} />}
+                        <img 
+                            className="fav" 
+                            src={favorites[index] ? Favori : Favori2} 
+                            alt="Favori" 
+                            onClick={() => toggleFavorite(index)}
+                        />
                         <h3>{recipe.title}</h3>
                         <p>Temps de préparation: {recipe.timing}</p>
                         <button>Voir plus</button>                        
