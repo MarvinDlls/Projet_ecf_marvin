f109448cd747b15aa31f44bdbba9bbf234159769
Marvindlls <marvindillenseger@gmail.com>
2024-12-13 09:53:55 +0100

    update API endpoint to use production URL

--- front/src/Utils/Api.jsx
+++ front/src/Utils/Api.jsx
@@ -1,5 +1,5 @@
 const fetchCategories = async () => {
-    const response = await fetch("http://localhost:3001/api/infos");
+    const response = await fetch("https://back-1-2yao.onrender.com/api/infos");
     const data = await response.json();
 
     return data;
