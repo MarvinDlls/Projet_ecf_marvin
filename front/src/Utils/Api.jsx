0b3e745251dee1047c3464422c8823ebb5cda1ff
Marvindlls <marvindillenseger@gmail.com>
2024-12-16 14:43:40 +0100

    simplify fetchCategories function by removing error handling

--- front/src/Utils/Api.jsx
+++ front/src/Utils/Api.jsx
@@ -1,15 +1,10 @@
 const fetchCategories = async () => {
-    try {
-        const response = await fetch("https://back-eynq.vercel.app/api/infos");
-        
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        
-        const data = await response.json();
-        console.log("Fetched data:", data);
-        return data;
-    } catch (error) {
-        console.error("Error fetching categories:", error);
-    }
-};
+    const response = await fetch("https://back-eynq.vercel.app/api/infos");
+    const data = await response.json();
+
+    return data;
+}
+
+export {
+    fetchCategories,
+}
