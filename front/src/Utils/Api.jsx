71809d3600cbb1414ed9b83c2565cd51586cd93a
Marvindlls <marvindillenseger@gmail.com>
2024-12-16 14:36:33 +0100

    enhance fetchCategories function with error handling for HTTP response

--- front/src/Utils/Api.jsx
+++ front/src/Utils/Api.jsx
@@ -1,7 +1,10 @@
 const fetchCategories = async () => {
     try {
         const response = await fetch("https://back-eynq.vercel.app/api/infos");
-    
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
         
         const data = await response.json();
         console.log("Fetched data:", data);
