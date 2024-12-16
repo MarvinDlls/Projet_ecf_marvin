d5dcf72ab43d70b6593f8fa20aac53c8ba5bc8b4
Marvindlls <marvindillenseger@gmail.com>
2024-12-16 14:32:31 +0100

    enhance fetchCategories function with error handling and logging

--- front/src/Utils/Api.jsx
+++ front/src/Utils/Api.jsx
@@ -1,10 +1,15 @@
 const fetchCategories = async () => {
-    const response = await fetch("https://back-eynq.vercel.app/api/infos");
-    const data = await response.json();
-
-    return data;
-}
-
-export {
-    fetchCategories,
-}
+    try {
+        const response = await fetch("https://back-eynq.vercel.app/api/infos");
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        console.log("Fetched data:", data);
+        return data;
+    } catch (error) {
+        console.error("Error fetching categories:", error);
+    }
+};
