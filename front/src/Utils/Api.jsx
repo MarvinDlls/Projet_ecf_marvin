c48c470e636158e673e503619daf315d541406db
Marvindlls <marvindillenseger@gmail.com>
2024-12-16 14:34:33 +0100

    refactor fetchCategories function to remove unnecessary error handling

--- front/src/Utils/Api.jsx
+++ front/src/Utils/Api.jsx
@@ -1,10 +1,7 @@
 const fetchCategories = async () => {
     try {
         const response = await fetch("https://back-eynq.vercel.app/api/infos");
-        
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+    
         
         const data = await response.json();
         console.log("Fetched data:", data);
